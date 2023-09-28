f349049877b5c91e8fd194c7aa402c5c1373f9d9
skybound0532 <tianchenggu@outlook.com>
2023-09-28 14:15:10 -0500

    finish gradebook

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-. . .
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,29 +12,58 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> grades = new HashMap<>();
+
+        String name;
+        String grade;
 
         boolean done = false;
         while(!done)
         {
             System.out.println("A)dd R)emove M)odify P)rint Q)uit");
             String input = in.next().toUpperCase();
+            
+            in.nextLine();
+
             if (input.equals("Q"))
             {
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                
+                System.out.print("Please enter the name: ");
+                name = in.nextLine();
+
+                System.out.print("Please enter grade: ");
+                grade = in.nextLine().toUpperCase();
+
+                grades.put(name, grade);
 
             } else if (input.equals("R"))
             {
-                . . .
+                System.out.print("Please enter the name: ");
+                
+                grades.remove(in.nextLine());
+
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.print("Please enter the name: ");
+                name = in.nextLine();
+
+                System.out.print("Please enter grade: ");
+                grade = in.nextLine().toUpperCase();
+
+                grades.put(name, grade);
+
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> names = grades.keySet();
+
+                for(String n : names)
+                {
+                    System.out.println(n + " " + grades.get(n));
+                }
+
             } else
             {
                 done = true;
