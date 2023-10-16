65617d8a081c2fdd26d28975245e5697b1a57d79
skybound0532 <tianchenggu@outlook.com>
2023-10-16 13:35:14 -0500

    Update

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,15 +1,39 @@
+
+
+import java.util.*;
+
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    Stack<Pair> theStack;
+    int count;
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        theStack = new Stack<>();
+        theStack.push(new Pair(row, column));
+        count = 1;
+        Pair current;
+
+        while(!theStack.isEmpty())
+        {
+            current = theStack.pop();
+            if(current.getRow() >= 0 && current.getCol() >= 0 && current.getRow() < 10 && current.getCol() < 10 && pixels[current.getRow()][current.getCol()] != 0)
+            {
+                pixels[current.getRow()][current.getCol()] = count;
+                count++;
+                theStack.push(new Pair(current.getRow()-1,current.getCol()));
+                theStack.push(new Pair(current.getRow(),current.getCol()-1));
+                theStack.push(new Pair(current.getRow()+1,current.getCol()));
+                theStack.push(new Pair(current.getRow(),current.getCol()+1));
+            }
+        }
+            
+        
     }
 
     @Override
---
+++ Chapter 15 Activities/FloodFill/src/Pair.java
@@ -0,0 +1,21 @@
+public class Pair
+{
+    private int row;
+    private int col;
+
+    public Pair(int r, int c)
+    {
+        row = r;
+        col = c;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+}
